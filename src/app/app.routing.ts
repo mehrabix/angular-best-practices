9e7820a3cce3d17514655ec5bcca1dfe299ab926
mehrabix <mehrabi@post.com>
2022-03-08 17:11:23 +0330

    router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryItem } from './myblog/pages/gallery-item/gallery-item.page';
 import { HomePage } from './myblog/pages/home.page';
 import { BlankLayout } from './shared/layouts/blank/blank.layout';
 import { MasterLayout } from './shared/layouts/master/master.layout';
@@ -20,6 +21,18 @@ const routes: Routes = [
             },
         ],
     },
+    {
+        path: 'gallery',
+        component: BlankLayout,
+        children:[
+            {
+                path: '',
+                component:GalleryItem,
+                
+                
+            }
+        ]
+    }
 ];
 
 @NgModule({
---
+++ src/app/myblog/pages/gallery-item/gallery-item.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-gallery-item',
+    templateUrl: './gallery-item.page.html',
+})
+export class GalleryItem implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
