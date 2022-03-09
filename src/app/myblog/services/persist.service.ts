1272b06db3917e49065832b3a3d76680751b5811
mehrabix <mehrabi@post.com>
2022-03-09 15:15:14 +0330

    sa

--- src/app/myblog/services/persist.service.ts
+++ src/app/myblog/services/persist.service.ts
@@ -9,21 +9,18 @@ export class PersistService {
 
 
 
-    public cypherKey: string = "qKSikFr*a#aU263Aqi6nJqi!Zt6";
-
-    
+    public _cypherKey: string = "qKSikFr*a#aU263Aqi6nJqi!Zt6";
 
 
-    
     set(key: string, data: any) {
         
-        const _encryptedData = CryptoJS.AES.encrypt(data,this.cypherKey.trim())
+        const _encryptedData = CryptoJS.AES.encrypt(data,this._cypherKey.trim())
         return localStorage.setItem(key, _encryptedData.toString())
     }
     get(key: string) {
         let _ls:any = localStorage.getItem(key)
         console.log(_ls)
-        const _decryptedKey = CryptoJS.AES.decrypt(_ls,this.cypherKey.trim()).toString(CryptoJS.enc.Utf8)
+        const _decryptedKey = CryptoJS.AES.decrypt(_ls,this._cypherKey.trim()).toString(CryptoJS.enc.Utf8)
        
         return _decryptedKey ? _decryptedKey.toString() : ""
     }
